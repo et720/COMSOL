4a2126364c528f69b8d9c4e0e5aa6d00ff72b5ea
et720 <74570778+et720@users.noreply.github.com>
2021-07-03 16:26:53 +0100

    Update Create_Your_File_Here.py

--- Second_Order_Reaction/Create_Your_File_Here.py
+++ Second_Order_Reaction/Create_Your_File_Here.py
@@ -3,86 +3,107 @@ import math
 #Blank line
 print('\n')
 
-#Injection volume
-volume = str(input("\033[1m" + 'Enter the total volume of reactants you are in injecting into the device (in litres)\nThen press Enter: ' + "\033[1m"))
-
-while volume.isdigit() == False:
-    volume = str(input("\033[1m" + 'Please enter a numerical value: ' + "\033[1m"))
-else:
-    volume = int(volume)
+#Injection Volume
+while True:
+    volume = input("\033[1m" + 'Enter the total of reactants you are using (in litres).' +
+                       '\nThen press Enter: ' + "\033[1m")
+    try:
+        volume = int(volume)
+        break
+    except ValueError:
+        print("\033[1m" + '\nPlease enter a numerical value. \n ' + "\033[1m")
+        continue
 
 #Calculate height required
 height = volume/(2E-9)
 
 #Blank line
 print('\n')
-
 print("For the following reaction:  " + "\033[1m" + "A + B --> C" + "\033[1m")
 
 #Concentration A
-A0 = str(input("\033[1m" + 'Enter your initial concentration of reactant A (in molarity), \nThen press Enter: ' + "\033[1m"))
-
-while A0.isdigit() == False:
-    A0 = str(input("\033[1m" + 'Please enter a numerical value: ' + "\033[1m"))
-else:
-    A0 = int(A0)
+while True:
+    A0 = input("\033[1m" +
+               'Enter your initial concentration of reactant A (in molarity), \nThen press Enter: ' +
+               "\033[1m")
+    try:
+        A0 = int(A0)
+        break
+    except ValueError:
+        print("\033[1m" + '\nPlease enter a numerical value. \n ' + "\033[1m")
+        continue
 
 #Blank line
 print('\n')
 
 #Concentration B
-B0 = str(input("\033[1m" + 'Enter your initial concentration of reactant B (in molarity), \nThen press Enter: ' + "\033[1m"))
-
-while B0.isdigit() == False:
-    B0 = str(input("\033[1m" + 'Please enter a numerical value: ' + "\033[1m"))
-else:
-    B0 = int(B0)
+while True:
+    B0 = input("\033[1m" +
+               'Enter your initial concentration of reactant B (in molarity), \nThen press Enter: ' +
+               "\033[1m")
+    try:
+        B0 = int(B0)
+        break
+    except ValueError:
+        print("\033[1m" + '\nPlease enter a numerical value. \n ' + "\033[1m")
+        continue
 
 #Blank line
 print('\n')
 
 #Diffusivity A
-A_diff = str(input("\033[1m" + 'Enter the diffusivity of A (in m^2/s), \nThen press Enter: ' + "\033[1m"))
-
-while A_diff.isdigit() == False:
-    A_diff = str(input("\033[1m" + 'Please enter a numerical value: ' + "\033[1m"))
-else:
-    A_diff = int(A_diff)
+while True:
+    A_diff = input("\033[1m" + 'Enter the diffusivity of A (in m^2/s), \nThen press Enter: '+
+               "\033[1m")
+    try:
+        A_diff = int(A_diff)
+        break
+    except ValueError:
+        print("\033[1m" + '\nPlease enter a numerical value. \n ' + "\033[1m")
+        continue
 
 #Blank line
 print('\n')
 
 #Diffusivity B
-B_diff = str(input("\033[1m" + 'Enter the diffusivity of B (in m^2/s), \nThen press Enter: ' + "\033[1m"))
+while True:
+    B_diff = input("\033[1m" + 'Enter the diffusivity of B (in m^2/s), \nThen press Enter: '+
+               "\033[1m")
+    try:
+        B_diff = int(B_diff)
+        break
+    except ValueError:
+        print("\033[1m" + '\nPlease enter a numerical value. \n ' + "\033[1m")
+        continue
 
-while B_diff.isdigit() == False:
-    B_diff = str(input("\033[1m" + 'Please enter a numerical value: ' + "\033[1m"))
-else:
-    B_diff = int(B_diff)
-    
 #Blank line
 print('\n')
 
 #Diffusivity C
-C_diff = str(input("\033[1m" + 'Enter the diffusivity of C (in m^2/s), \nThen press Enter: ' + "\033[1m"))
+while True:
+    C_diff = input("\033[1m" + 'Enter the diffusivity of C (in m^2/s), \nThen press Enter: '+
+               "\033[1m")
+    try:
+        C_diff = int(C_diff)
+        break
+    except ValueError:
+        print("\033[1m" + '\nPlease enter a numerical value. \n ' + "\033[1m")
+        continue
 
-while C_diff.isdigit() == False:
-    C_diff = str(input("\033[1m" + 'Please enter a numerical value: ' + "\033[1m"))
-else:
-    C_diff = int(C_diff)
-
-               
 #Blank line
 print('\n')
 
 #Rate Constant
-k = str(input("\033[1m" + 'Enter the rate constant (in M^-1.s^1) , \nThen press Enter: ' + "\033[1m"))
-
-while k.isdigit() == False:
-    k = str(input("\033[1m" + 'Please enter a numerical value: ' + "\033[1m"))
-else:
-    k = int(k)
-
+while True:
+    k = input("\033[1m" + 'Enter the rate constant (in M^-1.s^1) , \nThen press Enter: ' +
+                   "\033[1m")
+    try:
+        k = int(k)
+        break
+    except ValueError:
+        print("\033[1m" + '\nPlease enter a numerical value. \n ' + "\033[1m")
+        continue
+    
 #Blank line
 print('\n')
 
@@ -97,7 +118,6 @@ else:
     name = name + ".txt"
 
 txt_file  = open(name, "w+")
-
 txt_file.write("height  " + str(height) + "[um]  Height of truncated cone\n" )
 txt_file.write("A0  " + str(A0) + "[M]  Initial concentration A\n" )
 txt_file.write("B0  " + str(B0) + "[M]  Initial concentration B\n" )
@@ -115,8 +135,9 @@ print('\n')
 print("\033[1m" + "Find your file is here:" + "\033[1m")
 print("YourFilePath/COMSOL-main/Second_Order_Reaction")
 print('\n')
-print("\033[1m" + "Exiting...." + "\033[1m")
+print("\033[32;1m" + "Exiting...." + "\033[32;1m")
 
     
 exit()
+
 
